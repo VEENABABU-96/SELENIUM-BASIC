e460ca82de628a7eda74e787359294ff7dc10285
VEENABABU-96 <veenababuj16@gmail.com>
2024-05-08 22:18:36 +0530

    INITIAL COMMIT

---
+++ src/main/java/org/selenium/basics/FirefoxBrowserLaunch.java
@@ -0,0 +1,13 @@
+package org.selenium.basics;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirefoxBrowserLaunch {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.seleniumeasy.com/");
+		driver.manage().window().maximize();
+	}
+
+}
