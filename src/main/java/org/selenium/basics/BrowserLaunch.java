11709afae48e561dd09d876ebef6c7037822ec21
VEENABABU-96 <veenababuj16@gmail.com>
2024-05-09 23:06:34 +0530

    COMMANDS

--- src/main/java/org/selenium/basics/BrowserLaunch.java
+++ src/main/java/org/selenium/basics/BrowserLaunch.java
@@ -8,6 +8,15 @@ public class BrowserLaunch {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.seleniumeasy.com/");
 		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String currenturl = driver.getCurrentUrl();
+		System.out.println(currenturl);
+		String handleid = driver.getWindowHandle();
+		System.out.println(handleid);
+		String pagesource = driver.getPageSource();
+		System.out.println(pagesource);
+		driver.close();
 	}
 
 }
--- src/main/java/org/selenium/basics/FirefoxBrowserLaunch.java
+++ src/main/java/org/selenium/basics/FirefoxBrowserLaunch.java
@@ -8,6 +8,15 @@ public class FirefoxBrowserLaunch {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.seleniumeasy.com/");
 		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String currenturl = driver.getCurrentUrl();
+		System.out.println(currenturl);
+		String handleid = driver.getWindowHandle();
+		System.out.println(handleid);
+		String pagesource = driver.getPageSource();
+		System.out.println(pagesource);
+		driver.close();
 	}
 
 }
